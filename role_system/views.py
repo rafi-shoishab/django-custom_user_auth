2821015c20557b70e43ad1387c263325d88f0720
engr-rafiur <rsrafirahman@gmail.com>
2026-03-10 02:26:46 +0600

    index, settings done

--- role_system/views.py
+++ role_system/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render (request, 'role_system/index.html')
+
+def employer(request):
+    return render (request, 'role_system/employer.html')
